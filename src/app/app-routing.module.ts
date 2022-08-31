3c17a13d8b9bf4fa00f44e37fd0bb2092c5dc4f2
pelaosniper <101678457+pelaosniper@users.noreply.github.com>
2022-08-30 20:19:11 -0400

    creacion pagina principal
    ....

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'principal',
+    loadChildren: () => import('./principal/principal.module').then( m => m.PrincipalPageModule)
   },
+
 ];
 
 @NgModule({
