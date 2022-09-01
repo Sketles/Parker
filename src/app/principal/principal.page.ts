29e25d3b9cb344709b67a876ed5d43a7909e4852
Dante Velasquez <dan.velasquez@duocuc.cl>
2022-09-01 00:19:26 -0400

    Interpolacion
    Interpolacion

--- src/app/principal/principal.page.ts
+++ src/app/principal/principal.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalPage implements OnInit {
 
-  constructor() { }
+  data: any;
+
+  constructor(private activeroute: ActivatedRoute, private router: Router) {
+    // Se llama a la ruta activa y se obtiene sus parametros mediante una subscripcion
+    this.activeroute.queryParams.subscribe(params => { // Utilizamos lambda
+      if (this.router.getCurrentNavigation().extras.state) { // Validamos que en la navegacion actual tenga extras
+        this.data = this.router.getCurrentNavigation().extras.state.user; // Si tiene extra rescata lo enviado
+        console.log(this.data) // Muestra por consola lo traido
+      }else{this.router.navigate(["/principal"])} // Si no tiene extra la navegacion actual navegar al login
+    });
+  }
 
   ngOnInit() {
   }
-
 }
