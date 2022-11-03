9b67bbb7534473f5f304511afc95b3b7114bec67
Dante Velasquez <dan.velasquez@duocuc.cl>
2022-11-03 05:59:42 -0300

    v5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGardService } from './servicios/auth-gard.service';
-
 
 const routes: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { IonicStorageModule } from '@ionic/storage-angular'; //import del LOCAL-STORAGE
+import { IonicStorageModule } from '@ionic/storage-angular'; //import del STORAGE
 import { HttpClientModule } from '@angular/common/http'; //import del API HTTP
-import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx'; //Import del SQLITE
-import { DBTaskService } from './servicios/dbtask.service';
-import { AuthGardService } from './servicios/auth-gard.service';
-import { AuthenticationService } from './servicios/authentication.service';
+
 
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
   imports: [
     CommonModule,
     BrowserModule, 
@@ -27,7 +23,7 @@ import { AuthenticationService } from './servicios/authentication.service';
     IonicStorageModule.forRoot(),
     
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
   
 })
