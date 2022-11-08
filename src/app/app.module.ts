fe4f6b86f30e323bb6e5512543d6da142ed26e41
Dante Velasquez <dan.velasquez@duocuc.cl>
2022-11-07 21:42:08 -0300

    Rollback Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
 
 import { HttpClientModule } from '@angular/common/http'; //import del API HTTP
 
@@ -14,11 +13,10 @@ import { HttpClientModule } from '@angular/common/http'; //import del API HTTP
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    CommonModule,
     BrowserModule, 
     IonicModule.forRoot(), 
     AppRoutingModule,
-    HttpClientModule,    
+    HttpClientModule,  
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
