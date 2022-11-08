cf58989dd4e0a6fd515014b34fd598810a1021d1
Dante Velasquez <dan.velasquez@duocuc.cl>
2022-11-08 02:55:17 -0300

    Implementacion API Google Maps
    Implementacion API Google Maps

--- src/app/Paginas/home/home.page.ts
+++ src/app/Paginas/home/home.page.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ViewChild, ElementRef } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-home',
@@ -8,9 +11,24 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HomePage {
 
+  map: any;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
   constructor() { }
 
-  ngOnInit() {
+  ionViewDidEnter(){
+    this.showMap();
+  }
+
+  showMap(){
+    const location = new google.maps.LatLng(-33.36355749279399, -70.67821198064264);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
   }
   
 }
